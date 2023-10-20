17d2f3712034f9b86248a640a5403c234748022d
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-10-20 13:40:16 +0530

    structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { dark } from "@clerk/themes";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +22,26 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
-    appearance={{
-      baseTheme: dark,
-    }}
-  >
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
